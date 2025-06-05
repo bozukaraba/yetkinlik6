fc24b62e8b006b622fe8ba4b7404d87b8a5e9a2d
Adınız Soyadınız <email@adresiniz.com>
2025-06-06 00:08:20 +0300

    Fix CSS asset import for background image

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name) {
-            const info = assetInfo.name.split('.');
-            const extType = info[info.length - 1];
-            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-              return `assets/[name].[hash][extname]`;
-            }
-          }
-          return `assets/[name].[hash][extname]`;
-        },
-      },
-    },
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
