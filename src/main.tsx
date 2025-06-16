3fb2f16b2e87d286346dbdb3f5a15cc0aec3679e
ugur.kotbas <ugur.kotbas@turksat.com.tr>
2025-06-16 16:06:28 +0300

    Gereksiz dosyalar temizlendi: Firebase debug dosyaları ve config dosyaları silindi

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Debug fonksiyonlarını import et
-import './debug/checkAdmin.ts'
-import './debug/testSave.ts'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
